d2ea009db7a270b61aaa582df375006e677c2a6f
widyadharma <gede.widya.dharma@gmail.com>
2026-02-07 10:47:39 +0800

    perbaikan prod

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes; // <-- DIIMPOR
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    use HasFactory, SoftDeletes; // <-- DIGUNAKAN
+    use HasFactory, SoftDeletes;
 
     public $timestamps = false;
 
@@ -16,12 +16,8 @@ class Category extends Model
         'user_id',
         'name',
         'icon',
-        'color',
     ];
 
-    /**
-     * Get the user that owns the category (if it's not a default one).
-     */
     public function user()
     {
         return $this->belongsTo(User::class);
