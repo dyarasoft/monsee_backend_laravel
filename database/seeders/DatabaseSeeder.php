b23c51f4073c71ec07517b9c7bd10d966a312842
widyadharma <gede.widya.dharma@gmail.com>
2026-02-08 21:42:21 +0800

    Config Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,9 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Panggil seeder lain di sini
         $this->call([
             CategorySeeder::class,
+        ]);
+        $this->call([
+            ConfigSeeder::class,
         ]);
     }
 }
